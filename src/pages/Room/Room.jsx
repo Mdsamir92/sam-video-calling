931a973c394ec16eafe9d1bece5f7241ff0b83e3
Mohammad Samir <87142605+Mdsamir92@users.noreply.github.com>
2024-06-06 12:42:18 +0530

    Update Room.jsx

--- src/pages/Room/Room.jsx
+++ src/pages/Room/Room.jsx
@@ -23,7 +23,7 @@ export default function Room() {
             container: element,
             sharedLinks: [{
                 name: 'Copy Link',
-                url: `http://localhost:5173/room/${roomId}`
+                url: `https://sam-video-calling.vercel.app/room/${roomId}`
             }],
             scenario: {
                 mode: ZegoUIKitPrebuilt.OneONoneCall
